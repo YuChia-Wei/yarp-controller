1895a8e3f29f1317fc9b6b9f061fdf4e57fac9e6
YuChia <h42279@gmail.com>
2024-07-21 11:43:27 +0800

    add gateway unit testing

---
+++ src/Gateway/Yarp.Gateway.Tests/Authentication/AuthenticationBuilderExtensionTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Yarp.Gateway.Authentication;
+using Yarp.Gateway.Authentication.Options;
+
+namespace Yarp.Gateway.Tests.Authentication;
+
+[TestFixture]
+[TestOf(typeof(AuthenticationBuilderExtension))]
+public class AuthenticationBuilderExtensionTest
+{
+    private ServiceCollection _serviceCollection;
+    private GatewayAuthConfiguration? _gatewayAuthSettingOptions;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._serviceCollection = new ServiceCollection();
+    }
+
+    [Test]
+    public void AddYarpAuthentication_GivenJwtOptions_ShouldSuccess()
+    {
+        this._gatewayAuthSettingOptions = new GatewayAuthConfiguration
+        {
+            Default = DefaultAuthMethod.Jwt, Jwt = new JwtAuthConfiguration { Authority = "", Audience = "" }
+        };
+
+        this._serviceCollection.AddYarpAuthentication(this._gatewayAuthSettingOptions);
+
+        var buildServiceProvider = this._serviceCollection.BuildServiceProvider();
+
+        var authenticationSchemeProvider = buildServiceProvider.GetService<IAuthenticationSchemeProvider>();
+
+        authenticationSchemeProvider.Should().NotBeNull();
+    }
+
+    [Test]
+    public void AddYarpAuthentication_GivenNullOptions_ThrowException()
+    {
+        this._gatewayAuthSettingOptions = null;
+
+        var func = () => this._serviceCollection.AddYarpAuthentication(this._gatewayAuthSettingOptions);
+
+        func.Should().Throw<ArgumentNullException>();
+    }
+}
---
+++ src/Gateway/Yarp.Gateway.Tests/ConfigurationTests.cs
@@ -0,0 +1,85 @@
+﻿using System.IO;
+using System.Text;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using Yarp.Gateway.Authentication.Options;
+
+namespace Yarp.Gateway.Tests;
+
+[TestFixture]
+public class ConfigurationTests
+{
+    [Test]
+    public void ConfigurationSection_GetAuthConfiguration_Jwt_Success()
+    {
+        // Arrange
+        var jsonConfig = """
+                         {
+                           "GatewayAuthSetting": {
+                             "Default": "Jwt",
+                             "Jwt": {
+                                 "Authority": "http://localhost:8080",
+                                 "Audience": "",
+                                 "RequireHttpsMetadata": true
+                             }
+                           }
+                         }
+                         """;
+
+        var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonConfig));
+        var configuration = new ConfigurationBuilder()
+                            .AddJsonStream(memoryStream)
+                            .Build();
+
+        // Act
+        var appSettings = configuration.GetSection(GatewayAuthConfiguration.JsonSectionName).Get<GatewayAuthConfiguration>();
+
+        // Assert
+        appSettings!.Default.Should().Be(DefaultAuthMethod.Jwt);
+        appSettings!.Opid.Should().BeNull();
+        appSettings!.Jwt.Should().NotBeNull();
+    }
+
+    [Test]
+    public void ConfigurationSection_GetAuthConfiguration_Opid_Success()
+    {
+        // Arrange
+        var jsonConfig = """
+                         {
+                           "GatewayAuthSetting": {
+                             "Default": "Opid",
+                             "Opid": {
+                               "ClientId": "test_client",
+                               "ClientSecret": "6ZyOGaalxmk0NIZCg9w81lIU9bxnDL4P",
+                               "Authority": "http://localhost:8080/realms/master/",
+                               "WebApiAudience": [
+                                 "profile",
+                                 "roles"
+                               ],
+                               "LoginApplicationName": "my_app",
+                               "LoginCookieName": "local_login",
+                               "LoginCookieDomain": "",
+                               "TicketStoreRedisServer": "localhost:6379",
+                               "RequireHttpsMetadata": false,
+                               "ResponseType": "code",
+                               "RefreshTokenAddress": "http://localhost:8080/realms/master/protocol/openid-connect/token"
+                             }
+                           }
+                         }
+                         """;
+
+        var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonConfig));
+        var configuration = new ConfigurationBuilder()
+                            .AddJsonStream(memoryStream)
+                            .Build();
+
+        // Act
+        var appSettings = configuration.GetSection(GatewayAuthConfiguration.JsonSectionName).Get<GatewayAuthConfiguration>();
+
+        // Assert
+        appSettings!.Default.Should().Be(DefaultAuthMethod.Opid);
+        appSettings!.Opid.Should().NotBeNull();
+        appSettings!.Jwt.Should().BeNull();
+    }
+}
